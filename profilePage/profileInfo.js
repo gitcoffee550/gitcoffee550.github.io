3e76f08f4eddbad7b8b7ba63ea2d2cad296fc0b5
chris <gitcoffee550@gmail.com>
2025-05-02 17:15:01 +0800

    feat: profile page add queryString

--- profilePage/profileInfo.js
+++ profilePage/profileInfo.js
@@ -1,30 +1,32 @@
 import { artists } from './artistData.js';
 
 async function loadProfileData() {
-      // 1. 查找 "林福全" 的 `data`
-      const artist = artists.find(a => a.name === "林福全");
+    const urlParams = new URLSearchParams(window.location.search);
+    const name = urlParams.get('name') || "林福全";
 
-      if (!artist) {
-          console.error("找不到畫家資料");
-          return;
-      }
-  
-      const data = artist.data;
-  
+    const artist = artists.find(a => a.name === name);
+
+    if (!artist) {
+        console.error("找不到畫家資料");
+        return;
+    }
+
+    const data = artist.data;
+
     document.getElementById('profileImage').src = data.userImageUrl;
     document.getElementById('name').textContent = data.name;
     document.getElementById('region').textContent = data.region;
     document.getElementById('year').textContent = data.year;
     document.getElementById('description').innerHTML = data.description;
     document.getElementById('description1').innerHTML = data.description1;
-  
+
     const timelineContainer = document.getElementById('timelineContainer');
     timelineContainer.innerHTML = ''; // 先清空，避免重複渲染
     data.timeline.forEach((item, index) => {
-  
+
         const timelineItem = document.createElement('div');
         timelineItem.className = `timeline-item ${index % 2 === 0 ? 'left' : 'right'}`;
-  
+
         // 生成HTML
         const descriptionsHtml = item.descriptionList.map(desc => {
             return `
@@ -37,7 +39,7 @@ async function loadProfileData() {
             </div>
             `;
         }).join('');
-  
+
         let detailButtonHtml = item.showDetail ? `<a href="#" class="see-detail-btn">SEE DETAIL</a>` : '';
         timelineItem.innerHTML = `
             
@@ -52,11 +54,10 @@ async function loadProfileData() {
         `;
         timelineContainer.appendChild(timelineItem);
     });
-  }
-  
-  
-  document.addEventListener('DOMContentLoaded', () => {
-      loadProfileData();
-  });
+}
 
- 
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadProfileData();
+});
+
